0cc18c42a7573925551f735be881956113f4f17b
kaufmann <kaufmann43@gmail.com>
2020-04-19 19:24:12 -0500

    Updates

---
+++ precache-manifest.65ad2b7b39bef09799cd2333ae225b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515033f185c80584d95d93e76385b3fa",
+    "url": "/jsnes-web/index.html"
+  },
+  {
+    "revision": "cc1a520a9e80f11cd2e7",
+    "url": "/jsnes-web/static/css/main.52a65611.chunk.css"
+  },
+  {
+    "revision": "629832511e48e06a7903",
+    "url": "/jsnes-web/static/js/2.d07d8f7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jsnes-web/static/js/2.d07d8f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1a520a9e80f11cd2e7",
+    "url": "/jsnes-web/static/js/main.29e5387b.chunk.js"
+  },
+  {
+    "revision": "ed2f65e043f20d310a43",
+    "url": "/jsnes-web/static/js/runtime-main.25097ec8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80a56eec12c5dac4d57fa82c18345681.js"
+  "/jsnes-web/precache-manifest.65ad2b7b39bef09799cd2333ae225b49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jsnes-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
